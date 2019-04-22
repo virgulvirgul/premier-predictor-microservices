1c227d0b9b60afe717a9492c47e8a9a0dd45d130
Chris Shepherd <christopher.shepherd@imaginecurve.com>
2019-04-22 22:11:02 +0100

    Added matchFacts and predictions structs

---
+++ src/common/model/prediction.go
@@ -0,0 +1,50 @@
+package model
+
+import "github.com/cshep4/premier-predictor-microservices/proto-gen/model/gen"
+
+type MatchPredictionSummary struct {
+	HomeWin int32 `json:"homeWin,omitempty"`
+	Draw    int32 `json:"draw,omitempty"`
+	AwayWin int32 `json:"awayWin,omitempty"`
+}
+
+func MatchPredictionSummaryFromGrpc(predictionSummary *model.MatchPredictionSummary) *MatchPredictionSummary {
+	return &MatchPredictionSummary{
+		HomeWin: predictionSummary.HomeWin,
+		Draw:    predictionSummary.Draw,
+		AwayWin: predictionSummary.AwayWin,
+	}
+}
+
+func MatchPredictionSummaryToGrpc(predictionSummary *MatchPredictionSummary) *model.MatchPredictionSummary {
+	return &model.MatchPredictionSummary{
+		HomeWin: predictionSummary.HomeWin,
+		Draw:    predictionSummary.Draw,
+		AwayWin: predictionSummary.AwayWin,
+	}
+}
+
+type Prediction struct {
+	UserId  string `json:"userId,omitempty"`
+	MatchId string `json:"matchId,omitempty"`
+	HGoals  int32  `json:"hGoals,omitempty"`
+	AGoals  int32  `json:"aGoals,omitempty"`
+}
+
+func PredictionFromGrpc(prediction *model.Prediction) *Prediction {
+	return &Prediction{
+		UserId:  prediction.UserId,
+		MatchId: prediction.MatchId,
+		HGoals:  prediction.HGoals,
+		AGoals:  prediction.AGoals,
+	}
+}
+
+func PredictionToGrpc(prediction *Prediction) *model.Prediction {
+	return &model.Prediction{
+		UserId:  prediction.UserId,
+		MatchId: prediction.MatchId,
+		HGoals:  prediction.HGoals,
+		AGoals:  prediction.AGoals,
+	}
+}
